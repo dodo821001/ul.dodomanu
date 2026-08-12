08efc6cdf3933ee253962d4c7e4f6c6f5c4344a8
dodo821001 <dodo@dodokr.com>
2026-08-12 14:16:14 +0900

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// Google Sheets 연결 설정
+// 구글시트에서 파일 > 공유 > 웹에 게시 > CSV 형식으로 게시한 주소를 아래에 붙여넣으세요.
+// 비워두면 menu-fallback.csv를 사용합니다.
+window.DODO_MENU_CONFIG = {
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQXgmMFAW9NR3oPdICs1uWj1uuGL5Hc6gC8Rv8wieMr1JCyCj1pQ58H9HroUgsqP-WYSJO2wNPWFFRZ/pub?gid=528769608&single=true&output=csv",
+  STORE_NAME: "만화카페 도도",
+  SUBTITLE: "메뉴 사진과 가격을 한눈에 확인하세요"
+};
